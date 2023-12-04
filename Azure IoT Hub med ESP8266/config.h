fcb7dd14441c6142bb94806f9099b51d66197036
Rana  Alwan <112889332+ranaalwan@users.noreply.github.com>
2023-12-04 17:38:15 +0100

    Add files via upload

---
+++ Azure IoT Hub med ESP8266/config.h
@@ -0,0 +1,13 @@
+// config.h
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "WIFI_SSID"
+#define IOT_CONFIG_WIFI_PASSWORD "WIFI_PASSWORD"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "[your Azure IoT host name].azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "Device ID"
+#define IOT_CONFIG_DEVICE_KEY "Device Key"
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
